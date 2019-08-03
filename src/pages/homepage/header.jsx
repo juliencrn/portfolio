aee4a98fff2cc9725fe6f5c6fd252d9319bcd1a4
Julien <juliencaron18@gmail.com>
2019-08-03 20:18:04 +0200

    fix header

--- src/pages/homepage/header.jsx
+++ src/pages/homepage/header.jsx
@@ -10,69 +10,61 @@ import SplitWords from '../../components/splitWords'
 import Fade from '../../components/fade'
 import CoderSVG from '../../images/coder.svg'
 
-function SectionHeader({ title, textarea, subTitles, buttonLabel }) {
-  return (
-    <MediaQuery minWidth={breakpoints[1]}>
-      {notSmall => (
-        <Flex
-          as="section"
-          id="top"
-          alignItems="center"
-          style={{ minHeight: notSmall ? '100vh' : '60vh' }}
-        >
-          <Container py={6}>
+const SectionHeader = ({ title, textarea, subTitles, buttonLabel }) => (
+  <MediaQuery minWidth={breakpoints[1]}>
+    {notSmall => (
+      <Flex
+        as="section"
+        id="top"
+        alignItems="center"
+        style={{ minHeight: notSmall ? '100vh' : '60vh' }}
+      >
+        <Container py={6}>
+          <Flex mx={-3} justifyContent="space-between" alignItems="center">
+            <Box width={[1, 1, 1, 7 / 12]} px={3} alignItems="center">
+              <Fade>
+                <Heading as="h2" pb={3} fontSize={[5, 5, 6]}>
+                  {title}
+                </Heading>
+              </Fade>
+              <Fade>
+                <Heading as="h1" pb={4} fontSize={[6, 6, 7]}>
+                  <SplitWords words={subTitles} />
+                </Heading>
+              </Fade>
+              <Fade>
+                <div style={{ maxWidth: '600px' }}>
+                  <Html __html={textarea} />
+                </div>
+              </Fade>
+              <Fade>
+                <Box py={3}>
+                  <Button as={ScrollLink} to="contact" smooth isDynamic>
+                    {buttonLabel}
+                  </Button>
+                </Box>
+              </Fade>
+            </Box>
             <MediaQuery minWidth={breakpoints[2]}>
-              {large => (
-                <Flex mx={-3} justifyContent="space-between">
-                  <Flex width={large ? 7 / 12 : 1} px={3} alignItems="center">
-                    <Box width="100%">
-                      <Fade>
-                        <Heading as="h2" pb={3} fontSize={[5, 5, 6]}>
-                          {title}
-                        </Heading>
-                      </Fade>
-                      <Fade>
-                        <Heading as="h1" pb={4} fontSize={[6, 6, 7]}>
-                          <SplitWords words={subTitles} />
-                        </Heading>
-                      </Fade>
-                      <Fade>
-                        <div style={{ maxWidth: '600px' }}>
-                          <Html __html={textarea} />
-                        </div>
-                      </Fade>
-                      <Fade>
-                        <Box py={3}>
-                          <Button as={ScrollLink} to="contact" smooth isDynamic>
-                            {buttonLabel}
-                          </Button>
-                        </Box>
-                      </Fade>
-                    </Box>
-                  </Flex>
-                  {large && (
-                    <Flex
-                      width={5 / 12}
-                      px={3}
-                      justifyContent="center"
-                      alignItems="center"
-                    >
-                      <Box width="100%" pa={3}>
-                        <Fade>
-                          <CoderSVG width="100%" />
-                        </Fade>
-                      </Box>
-                    </Flex>
-                  )}
-                </Flex>
-              )}
+              <Flex
+                width={5 / 12}
+                px={3}
+                justifyContent="center"
+                alignItems="center"
+              >
+                <Box width="100%" pa={3}>
+                  <Fade>
+                    <CoderSVG width="100%" />
+                  </Fade>
+                </Box>
+              </Flex>
             </MediaQuery>
-          </Container>
-        </Flex>
-      )}
-    </MediaQuery>
-  )
-}
+          </Flex>
+        </Container>
+      </Flex>
+    )}
+  </MediaQuery>
+)
 
 SectionHeader.propTypes = {
   title: PropTypes.string,
