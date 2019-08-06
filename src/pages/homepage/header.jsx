da3e1b8bc182d15a4abd4aa56dd5519b72324d21
Julien <juliencaron18@gmail.com>
2019-08-07 01:18:25 +0200

    Fix header UI

--- src/pages/homepage/header.jsx
+++ src/pages/homepage/header.jsx
@@ -11,59 +11,55 @@ import Fade from '../../components/fade'
 import CoderSVG from '../../images/coder.svg'
 
 const SectionHeader = ({ title, textarea, subTitles, buttonLabel }) => (
-  <MediaQuery minWidth={breakpoints[1]}>
-    {matches => (
-      <Flex
-        as="section"
-        id="top"
-        alignItems="center"
-        style={{ minHeight: !matches ? '60vh' : '100vh' }}
-      >
-        <Container py={6}>
-          <Flex mx={-3} justifyContent="space-between" alignItems="center">
-            <Box width={[1, 1, 1, 7 / 12]} px={3} alignItems="center">
+  <Flex
+    as="section"
+    id="top"
+    alignItems="center"
+    style={{ minHeight: '100vh' }}
+  >
+    <Container py={6}>
+      <Flex pt={4} mx={-3} justifyContent="space-between" alignItems="center">
+        <Box width={[1, 1, 1, 7 / 12]} px={3} alignItems="center">
+          <Fade>
+            <Heading as="h2" pb={3} fontSize={[5, 5, 6]}>
+              {title}
+            </Heading>
+          </Fade>
+          <Fade>
+            <Heading as="h1" pb={4} fontSize={[6, 6, 7]}>
+              <SplitWords words={subTitles} />
+            </Heading>
+          </Fade>
+          <Fade>
+            <div style={{ maxWidth: '650px' }}>
+              <Html __html={textarea} />
+            </div>
+          </Fade>
+          <Fade>
+            <Box py={3}>
+              <Button as={ScrollLink} to="contact" smooth isDynamic>
+                {buttonLabel}
+              </Button>
+            </Box>
+          </Fade>
+        </Box>
+        <MediaQuery minWidth={breakpoints[2]}>
+          <Flex
+            width={5 / 12}
+            px={3}
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Box width="100%" pa={3}>
               <Fade>
-                <Heading as="h2" pb={3} fontSize={[5, 5, 6]}>
-                  {title}
-                </Heading>
-              </Fade>
-              <Fade>
-                <Heading as="h1" pb={4} fontSize={[6, 6, 7]}>
-                  <SplitWords words={subTitles} />
-                </Heading>
-              </Fade>
-              <Fade>
-                <div style={{ maxWidth: '650px' }}>
-                  <Html __html={textarea} />
-                </div>
-              </Fade>
-              <Fade>
-                <Box py={3}>
-                  <Button as={ScrollLink} to="contact" smooth isDynamic>
-                    {buttonLabel}
-                  </Button>
-                </Box>
+                <CoderSVG width="100%" height="100%" />
               </Fade>
             </Box>
-            <MediaQuery minWidth={breakpoints[2]}>
-              <Flex
-                width={5 / 12}
-                px={3}
-                justifyContent="center"
-                alignItems="center"
-              >
-                <Box width="100%" pa={3}>
-                  <Fade>
-                    <CoderSVG width="100%" height="100%" />
-                  </Fade>
-                </Box>
-              </Flex>
-            </MediaQuery>
           </Flex>
-        </Container>
+        </MediaQuery>
       </Flex>
-    )}
-  </MediaQuery>
+    </Container>
+  </Flex>
 )
 
 SectionHeader.propTypes = {
