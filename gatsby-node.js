4b7ee39719d9be339c893fb792722f0220a11178
Julien <juliencaron@protonmail.com>
2020-02-15 16:37:19 +0100

    exclude posts from node

--- gatsby-node.js
+++ gatsby-node.js
@@ -22,12 +22,21 @@ exports.createPages = async ({ graphql, actions }) => {
 
   const template = path.resolve('src/templates/post.tsx')
   pages.data.allPrismicPost.edges.forEach(edge => {
-    createPage({
-      path: `/${edge.node.uid}`,
-      component: template,
-      context: {
-        uid: edge.node.uid
-      }
-    })
+    // Have a graphQL error if has a empty field
+    // To fix it, we have a dummy post with all fields
+    // Here we dont' create page for this.
+    const hiddenPostSlugs = [
+      'bonjour-cher-visiteur-bienvenue-sur-mon-article-demo'
+    ]
+
+    if (!hiddenPostSlugs.includes(edge.node.uid)) {
+      createPage({
+        path: `/${edge.node.uid}`,
+        component: template,
+        context: {
+          uid: edge.node.uid
+        }
+      })
+    }
   })
 }
