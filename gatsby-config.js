781022260d1adf814f6a36bfb72d130a74862fd4
Julien <juliencaron@protonmail.com>
2020-03-31 16:42:35 +0200

    Add robots.txt

--- gatsby-config.js
+++ gatsby-config.js
@@ -51,6 +51,14 @@ module.exports = {
       }
     },
     {
+      resolve: 'gatsby-plugin-robots-txt',
+      options: {
+        host: siteUrl,
+        sitemap: `${siteUrl}/sitemap.xml`,
+        policy: [{ userAgent: '*', allow: '/' }]
+      }
+    },
+    {
       resolve: `gatsby-plugin-feed`,
       options: {
         feeds: [
